8060153d7d10e9558df9997f4c2b2624380b14bb
Henrique Soares da Silva <soares.henrique495@gmail.com>
2024-03-27 03:02:55 -0300

    fixing debug

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -9,6 +9,7 @@
 return {
   -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
+  'nvim-neotest/nvim-nio',
   -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
